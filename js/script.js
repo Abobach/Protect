0d911cf1fb42e84a107c2f895a6d786d91a87d10
Artem <artemkumakshev@gmail.com>
2022-01-24 14:48:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+// const category = 'toys';
+
+// console.log(`ttps://someurl.com/${category}`);
+
+console.log('arr'+ "- obj");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2*4===8);
+// && и
+//|| или
+
+
+//alert('Hello');
+
+//const result = confirm("Are you here");
+//console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+//const answer = [];
+
+// answer[0] = prompt('как ваше имя?', '');
+// answer[2] = prompt('как ваше Фами?лия', '');
+// answer[3] = prompt('Сколько вам лет?', '');
+
+
+
+//Квалификация типов данных в JS
+//число
+// let numbera = 4;
+
+// //bool
+// const bool = true;
+
+// //null or undefind
+// //console.log(somthing);
+// let d;
+// console.log(d);
+// //Обьекты
+// const obja = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obja.name);
+// //massiv
+// let arr = ['plum.png', 'orange', 'apple'];
+
+// console.log(arr[1]);
+
+
+
+
+
+// let number = 5;
+// const leftBorderWith = 1;
+
+// number = 10;
+// console.log(number);
+
+//  const obj = {
+//      a: 50
+//  };
+//  obj.a = 10;
+//  console.log(obj);
